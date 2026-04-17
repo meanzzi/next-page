c3eb923e30b6f962bed0a8a4c7cf443dfda7a7ff
meanzzi <meanzi.kim@gmail.com>
2026-04-17 22:29:51 +0900

    feat : UI 구현

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,22 @@
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import { ReactNode } from "react";
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
 
 // 루트 폴더 이므로 전체적으로 공통되는
 // 헤더 컴포넌트나 레이아웃 렌더링, 비즈니스 로직 작성
-export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const onClickButton = () => {
-    router.push("/test");
-  };
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & {
+  Component: NextPageWithLayout;
+}) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
 
-  return (
-    <>
-      <header>
-        <Link href="/">index</Link>
-        &nbsp;
-        <Link href="/search">search</Link>
-        &nbsp;
-        <Link href="/book/1">book/1</Link>
-        <div>
-          <button onClick={onClickButton}>/test 페이지 이동</button>
-        </div>
-      </header>
-      <Component {...pageProps} />
-    </>
-  );
+  return <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>;
 }
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,13 +1,18 @@
-// 쿼리 스트링 값을 읽어주기 위해서
-import { useRouter } from "next/router";
+import SearchableLayout from "@/components/searchable-layout";
+import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default function Page() {
-  const router = useRouter();
-  const { q } = router.query;
-
   return (
     <div>
-      <h1>Search {q}</h1>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
     </div>
   );
 }
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
